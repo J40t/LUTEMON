7ac37496990b4212dcb69a8331e0853dd14087ca
Jaot <jaot.walker@gmail.com>
2023-04-25 11:28:40 +0300

    Basic Storage created

---
+++ app/src/main/java/org/cadenzu/lutemon/Storage.java
@@ -0,0 +1,23 @@
+package org.cadenzu.lutemon;
+
+import java.util.ArrayList;
+
+public class Storage {
+    protected String name;
+    protected ArrayList<Lutemon> lutemons;
+
+    public Storage(String name, ArrayList<Lutemon> lutemons) {
+        this.name = name;
+        this.lutemons = lutemons;
+    }
+
+    public static Storage storage = null;
+
+    public Storage getInstance() {
+        if (storage == null) {
+            storage = new Storage(name, lutemons);
+        }
+        return storage;
+    }
+
+}
