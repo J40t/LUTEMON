e723c53135003853bd9620939697a3e7fe1533bb
Jaot <jaot.walker@gmail.com>
2023-04-27 17:14:14 +0300

    Added fragment bases Added semi-functional buttons

--- app/src/main/java/org/cadenzu/lutemon/MainActivity.java
+++ app/src/main/java/org/cadenzu/lutemon/MainActivity.java
@@ -1,8 +1,11 @@
 package org.cadenzu.lutemon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnHome = findViewById(R.id.btnHome);
+        Button btnTraining = findViewById(R.id.btnTrain);
+        Button btnDuel = findViewById(R.id.btnDuel);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragContainer, HomeFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("back")
+                        .commit();
+            }
+        });
+
+        btnTraining.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragContainer, TrainingFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("back")
+                        .commit();
+            }
+        });
+
+        btnDuel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragContainer, DuelFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("back")
+                        .commit();
+            }
+        });
 
     }
 }
