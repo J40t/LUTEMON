fa74c2f2a09694d0b72c96d2ac6e03ab2a43f8ff
Jaot <jaot.walker@gmail.com>
2023-05-02 12:22:27 +0300

    Fight: Semily implemented

--- app/src/main/java/org/cadenzu/lutemon/Fight.java
+++ app/src/main/java/org/cadenzu/lutemon/Fight.java
@@ -1,39 +1,95 @@
 package org.cadenzu.lutemon;
 
+import android.widget.RadioGroup;
+
 import org.cadenzu.lutemon.lutemon.Lutemon;
 
+import java.util.ArrayList;
+
 public class Fight {
+    private Lutemon lutemonAttacker;
+    private Lutemon lutemonDefender;
 
-    private Lutemon lutemon1;
-    private Lutemon lutemon2;
-
-
-    public Fight(Lutemon lutemon1, Lutemon lutemon2) {
-        this.lutemon1 = lutemon1;
-        this.lutemon2 = lutemon2;
+    public Fight() {
+        this.lutemonAttacker = DuelArena.getInstance().getLutemons().get(0);
+        this.lutemonDefender = DuelArena.getInstance().getLutemons().get(1);
     }
 
-    public void battle() {
+    public void fullBattle() {
 
+        while (true) {
+            //Samaan aikaan FightActivityssa:
+            //txtStats1.setText(fight.battleDisplayStatsLutemon(fight.getLutemon1()));
+            //txtStats2.setText(fight.battleDisplayStatsLutemon(fight.getLutemon2()));
+            lutemonDefender.setHealth(lutemonDefenderHealthCalculation());
+
+            if (lutemonDefender.getHealth() <= 0) {
+
+                break;
+            }
+
+
+        }
 
     }
 
-    public void displayStatsLutemon(Lutemon lutemon) {
+
+    public String battleDisplayStatsLutemon(Lutemon lutemon) {
         String stats = lutemon.getColor()
-                + "(" + lutemon.getName() + ")"
+                + "(" + lutemon.getName() + ") "
                 + "att: " + lutemon.getAttack() + "; "
                 + "def: " + lutemon.getDefense() + "; "
                 + "exp: " + lutemon.getExp() + "; "
                 + "health: " + lutemon.getHealth() + "/" + lutemon.getMaxHealth();
+        return stats;
+    }
+
+    public String attackEventMessage() {
+        String eventMessage = lutemonAttacker.getColor()
+                + " (" + lutemonAttacker.getName() + ") "
+                + "attacks "
+                + lutemonDefender.getColor()
+                + " (" + lutemonDefender.getName() + ")";
+
+        return eventMessage;
+    }
+
+    public int lutemonDefenderHealthCalculation() {
+        int newHealth = lutemonDefender.getHealth() - (lutemonDefender.getDefense() - lutemonAttacker.getAttack());
+        return newHealth;
     }
 
 
 
-    public Lutemon getLutemon1() {
-        return lutemon1;
+    public String surviveMessage(Lutemon lutemon) {
+        String survMessage = lutemon.getColor()
+                + "(" + lutemon.getName() + ")"
+                + "manages to escape death.";
+        return survMessage;
     }
 
-    public Lutemon getLutemon2() {
-        return lutemon2;
+
+
+    public void flipAttackerAndDefender() {
+        Lutemon newRoleLutemon = lutemonDefender;
+        setLutemonAttacker(newRoleLutemon); //Defender switches to attacker
+        setLutemonDefender(lutemonAttacker); //Attacker switches to defender
+
+    }
+
+    public Lutemon getLutemonAttacker() {
+        return lutemonAttacker;
+    }
+
+    public Lutemon getLutemonDefender() {
+        return lutemonDefender;
+    }
+
+    public void setLutemonAttacker(Lutemon lutemonAttacker) {
+        this.lutemonAttacker = lutemonAttacker;
+    }
+
+    public void setLutemonDefender(Lutemon lutemonDefender) {
+        this.lutemonDefender = lutemonDefender;
     }
 }
--- app/src/main/java/org/cadenzu/lutemon/FightActivity.java
+++ app/src/main/java/org/cadenzu/lutemon/FightActivity.java
@@ -7,19 +7,40 @@ import android.widget.TextView;
 
 public class FightActivity extends AppCompatActivity {
 
-    private TextView txtLutemonTitle1;
-    private TextView txtLutemonTitle2;
+    private TextView txtLutemonTitle1, txtLutemonTitle2;
+    private TextView txtStats1, txtStats2;
+
+    private TextView txtAttackEvent, txtResultEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fight);
 
+        Fight fight = new Fight();
+
         txtLutemonTitle1 = findViewById(R.id.txtLutemonTitle1);
         txtLutemonTitle2 = findViewById(R.id.txtLutemonTitle2);
+        txtStats1 = findViewById(R.id.txtStats1);
+        txtStats2 = findViewById(R.id.txtStats2);
+        txtAttackEvent = findViewById(R.id.txtAttackEvent);
+        txtResultEvent = findViewById(R.id.txtResultEvent);
 
-        txtLutemonTitle1.setText(fight.getLutemon1().getName());
-        txtLutemonTitle2.setText(fight.getLutemon2().getName());
+        txtLutemonTitle1.setText(fight.getLutemonAttacker().getName());
+        txtLutemonTitle2.setText(fight.getLutemonDefender().getName());
+
+
+
+
+        while (true) {
+
+            txtStats1.setText(fight.battleDisplayStatsLutemon(fight.getLutemonAttacker()));
+            txtStats2.setText(fight.battleDisplayStatsLutemon(fight.getLutemonDefender()));
+            txtAttackEvent.setText(fight.attackEventMessage());
+            break;
+
+        }
+
 
     }
 
